fbe4c5a2f52ffcf553be977c7ff9367f030b0bff
Phillippe Siclait <phillippe@clay3d.io>
2022-12-01 09:05:46 -0500

    Day 1 Part 1

--- day-1/src/index.test.ts
+++ day-1/src/index.test.ts
@@ -1,5 +1,38 @@
-import { sum } from "./index"
+import { readFile } from "node:fs/promises"
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3)
+import { mostCalories, parseInput } from "./index"
+
+const expectedInventories = [
+  {
+    meals: [{ calories: 1000 }, { calories: 2000 }, { calories: 3000 }],
+  },
+  {
+    meals: [{ calories: 4000 }],
+  },
+  {
+    meals: [{ calories: 5000 }, { calories: 6000 }],
+  },
+  {
+    meals: [{ calories: 7000 }, { calories: 8000 }, { calories: 9000 }],
+  },
+  {
+    meals: [{ calories: 10000 }],
+  },
+]
+
+test("reads the input file", async () => {
+  const contents = await readFile("./resources/sample.txt")
+  expect(parseInput(contents.toString())).toEqual(expectedInventories)
+})
+
+test("calculates most calories for sample", async () => {
+  const contents = await readFile("./resources/sample.txt")
+  const input = parseInput(contents.toString())
+  expect(mostCalories(input)).toEqual(24_000)
+})
+
+test("calculates most calories for input", async () => {
+  const contents = await readFile("./resources/input.txt")
+  const input = parseInput(contents.toString())
+  expect(mostCalories(input)).toEqual(71_300)
 })
--- day-1/src/index.ts
+++ day-1/src/index.ts
@@ -1,3 +1,33 @@
-export function sum(a: number, b: number): number {
-  return a + b
+class Inventory {
+  meals: Meal[] = []
+
+  addMeal(calories: number) {
+    this.meals.push(new Meal(calories))
+  }
+
+  get totalCalories(): number {
+    return this.meals.reduce((total, meal) => total + meal.calories, 0)
+  }
+}
+
+class Meal {
+  constructor(readonly calories: number) {}
+}
+
+export function parseInput(input: string): Inventory[] {
+  const inventories: Inventory[] = []
+  let inventory = new Inventory()
+  for (const value of input.split("\n")) {
+    if (value === "") {
+      inventories.push(inventory)
+      inventory = new Inventory()
+      continue
+    }
+    inventory.addMeal(parseInt(value))
+  }
+  return inventories
+}
+
+export function mostCalories(inventories: Inventory[]): number {
+  return Math.max(...inventories.map((inventory) => inventory.totalCalories))
 }
