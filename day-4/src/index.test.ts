58843b9c40b78f7ac5cdf9c4ae4498db3e31c7d3
Phillippe Siclait <phillippe@clay3d.io>
2022-12-04 18:11:40 -0500

    Day 4 Part 2

--- day-4/src/index.test.ts
+++ day-4/src/index.test.ts
@@ -2,7 +2,7 @@
 
 import { readFile } from "node:fs/promises"
 
-import { fullyContainedPairs, SectionAssignment } from "./index"
+import { fullyContainedPairs, overlappingPairs, SectionAssignment } from "./index"
 
 describe("SectionAssignment", () => {
   test("2-4 does not contain 6-8", async () => {
@@ -20,6 +20,38 @@ describe("SectionAssignment", () => {
   test("4-6 contians 6-6", async () => {
     expect(new SectionAssignment(4, 6).contains(new SectionAssignment(6, 6))).toBeTruthy()
   })
+
+  test("2-4 does not overlap with 6-8", async () => {
+    expect(new SectionAssignment(2, 4).overlaps(new SectionAssignment(6, 8))).toBeFalsy()
+  })
+
+  test("6-8 does not overlap with 2-4", async () => {
+    expect(new SectionAssignment(6, 8).overlaps(new SectionAssignment(2, 4))).toBeFalsy()
+  })
+
+  test("2-3 does not overlap with 4-5", async () => {
+    expect(new SectionAssignment(2, 3).overlaps(new SectionAssignment(4, 5))).toBeFalsy()
+  })
+
+  test("5-7 overlaps with 7-9", async () => {
+    expect(new SectionAssignment(5, 7).overlaps(new SectionAssignment(7, 9))).toBeTruthy()
+  })
+
+  test("2-8 overlaps with 3-7", async () => {
+    expect(new SectionAssignment(2, 8).overlaps(new SectionAssignment(3, 7))).toBeTruthy()
+  })
+
+  test("6-6 overlaps with 4-6", async () => {
+    expect(new SectionAssignment(6, 6).overlaps(new SectionAssignment(4, 6))).toBeTruthy()
+  })
+
+  test("4-6 overlaps with 6-6", async () => {
+    expect(new SectionAssignment(4, 6).overlaps(new SectionAssignment(4, 6))).toBeTruthy()
+  })
+
+  test("2-6 overlaps with 4-8", async () => {
+    expect(new SectionAssignment(2, 6).overlaps(new SectionAssignment(4, 8))).toBeTruthy()
+  })
 })
 
 test("sample contains 2 fully contained pairs", async () => {
@@ -30,4 +62,14 @@ test("sample contains 2 fully contained pairs", async () => {
 test("input contains 582 fully contained pairs", async () => {
   const contents = await readFile("./resources/input.txt")
   expect(fullyContainedPairs(contents.toString())).toBe(582)
+})
+
+test("sample contains 4 overlapping pairs", async () => {
+  const contents = await readFile("./resources/sample.txt")
+  expect(overlappingPairs(contents.toString())).toBe(4)
+})
+
+test("input contains 766 overlapping pairs", async () => {
+  const contents = await readFile("./resources/input.txt")
+  expect(overlappingPairs(contents.toString())).toBe(893)
 })
--- day-4/src/index.ts
+++ day-4/src/index.ts
@@ -4,6 +4,12 @@ export function fullyContainedPairs(input: string): number {
     .reduce((sum, value) => sum + +value, 0)
 }
 
+export function overlappingPairs(input: string): number {
+  return readLines(input)
+    .map(isOverlappingPair)
+    .reduce((sum, value) => sum + +value, 0)
+}
+
 function readLines(input: string): string[] {
   const lines = input.split("\n")
   lines.pop()
@@ -13,6 +19,12 @@ function readLines(input: string): string[] {
 function isFullyContainedPair(line: string): boolean {
   const [assignment1, assignment2] = line.split(",").map((pair) => new SectionAssignment(...splitPair(pair)))
   return assignment1.contains(assignment2) || assignment2.contains(assignment1)
+}
+
+function isOverlappingPair(line: string): boolean {
+  const [assignment1, assignment2] = line.split(",").map((pair) => new SectionAssignment(...splitPair(pair)))
+  console.log(`1: ${assignment1}, 2: ${assignment2}, overlaps: ${assignment1.overlaps(assignment2)}`)
+  return assignment1.overlaps(assignment2)
 }
 
 function splitPair(pair: string): [number, number] {
@@ -25,5 +37,18 @@ export class SectionAssignment {
 
   contains(other: SectionAssignment): boolean {
     return this.left <= other.left && this.right >= other.right
+  }
+
+  overlaps(other: SectionAssignment): boolean {
+    return (
+      (this.left <= other.left && this.right >= other.left) ||
+      (this.left <= other.right && this.right >= other.right) ||
+      this.contains(other) ||
+      other.contains(this)
+    )
+  }
+
+  toString(): string {
+    return `[${this.left},${this.right}]`
   }
 }
